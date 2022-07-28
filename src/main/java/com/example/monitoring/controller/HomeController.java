0ba4f1a33f55a9f18690ef5127905fa56fd848a5
haind6 <haind6@vng.com.vn>
2022-07-28 14:30:52 +0700

    add timer counter

--- src/main/java/com/example/monitoring/controller/HomeController.java
+++ src/main/java/com/example/monitoring/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.example.monitoring.controller;
 
-
+import io.micrometer.core.annotation.Counted;
+import io.micrometer.core.annotation.Timed;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/home")
 public class HomeController {
+
+  @Timed
+  @Counted
   @GetMapping
   public String home() {
     return "hello";
